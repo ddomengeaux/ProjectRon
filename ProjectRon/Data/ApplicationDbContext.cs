ab8d554b61d1aee00f9b4e89ac01f7f8f54680ac
Daniel Domengeaux <danield@supa-fly.org>
2023-01-29 21:04:45 -0600

    initial commit

---
+++ ProjectRon/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectRon.Data;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser<Guid>, IdentityRole<Guid>, Guid>
+
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
+
